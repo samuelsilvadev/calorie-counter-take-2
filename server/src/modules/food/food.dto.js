a05a2dac5a737ada43d0bf31369d0bbabaee02c6
Samuel Silva <samuelsilvawb@gmail.com>
2022-01-22 13:24:46 +0000

    refactor(server): update model and dto to have correct fields

--- server/src/modules/food/food.dto.js
+++ server/src/modules/food/food.dto.js
@@ -1,36 +1,56 @@
 "use strict";
 
+function setProps(target, props) {
+  target.id = props._id;
+  target.createdBy = props.createdBy;
+  target.updatedBy = props.updatedBy;
+  target.saturatedFats = props.saturatedFats;
+  target.calories = props.calories;
+  target.alcohol = props.alcohol;
+  target.addedSugars = props.addedSugars;
+  target.solidFats = props.solidFats;
+  target.oils = props.oils;
+  target.dryBeansPeas = props.dryBeansPeas;
+  target.soy = props.soy;
+  target.meats = props.meats;
+  target.milk = props.milk;
+  target.fruits = props.fruits;
+  target.otherVegetables = props.otherVegetables;
+  target.starchyVegetables = props.starchyVegetables;
+  target.darkGreenVegetables = props.darkGreenVegetables;
+  target.orangeVegetables = props.orangeVegetables;
+  target.vegetables = props.vegetables;
+  target.wholeGrains = props.wholeGrains;
+  target.grains = props.grains;
+  target.multiplier = props.multiplier;
+  target.increment = props.increment;
+  target.factor = props.factor;
+  target.portionDisplayName = props.portionDisplayName;
+  target.portionAmount = props.portionAmount;
+  target.portion = props.portion;
+  target.name = props.name;
+}
+
 class GetDTO {
   constructor({ ...props }) {
-    this._id = props._id;
-    this.title = props.title;
-    this.content = props.content;
+    setProps(this, props);
 
-    // Auto Generated Fields
-    this.createdBy = props.createdBy;
-    this.updatedBy = props.updatedBy;
-    this.createdAt = props.createdAt;
-    this.updatedAt = props.updatedAt;
     Object.freeze(this);
   }
 }
 
 class InsertDTO {
   constructor({ ...props }) {
-    this.title = props.title;
-    this.content = props.content;
-    // Auto Generated Fields
-    this.createdBy = props.createdBy;
+    setProps(this, props);
+
     Object.freeze(this);
   }
 }
 
 class UpdateDTO {
   constructor({ ...props }) {
-    this.title = props.title;
-    this.content = props.content;
-    // Auto Generated Fields
-    this.updatedBy = props.updatedBy;
+    setProps(this, props);
+
     // Delete Fields which are not present in data
     Object.keys(this).forEach((key) => {
       if (this[key] === undefined) {
--- server/src/modules/food/food.model.js
+++ server/src/modules/food/food.model.js
@@ -7,12 +7,104 @@ class Food {
   initSchema() {
     const schema = new Schema(
       {
-        title: {
+        name: {
           type: String,
           required: true,
         },
-        content: {
+        portion: {
+          type: Number,
+          required: true,
+        },
+        portionAmount: {
+          type: Number,
+          required: true,
+        },
+        portionDisplayName: {
           type: String,
+          required: true,
+        },
+        factor: {
+          type: Number,
+          required: true,
+        },
+        increment: {
+          type: Number,
+          required: true,
+        },
+        multiplier: {
+          type: Number,
+          required: true,
+        },
+        grains: {
+          type: Number,
+          required: true,
+        },
+        wholeGrains: {
+          type: Number,
+          required: true,
+        },
+        vegetables: {
+          type: Number,
+          required: true,
+        },
+        orangeVegetables: {
+          type: Number,
+          required: true,
+        },
+        darkGreenVegetables: {
+          type: Number,
+          required: true,
+        },
+        starchyVegetables: {
+          type: Number,
+          required: true,
+        },
+        otherVegetables: {
+          type: Number,
+          required: true,
+        },
+        fruits: {
+          type: Number,
+          required: true,
+        },
+        milk: {
+          type: Number,
+          required: true,
+        },
+        meats: {
+          type: Number,
+          required: true,
+        },
+        soy: {
+          type: Number,
+          required: true,
+        },
+        dryBeansPeas: {
+          type: Number,
+          required: true,
+        },
+        oils: {
+          type: Number,
+          required: true,
+        },
+        solidFats: {
+          type: Number,
+          required: true,
+        },
+        addedSugars: {
+          type: Number,
+          required: true,
+        },
+        alcohol: {
+          type: Number,
+          required: true,
+        },
+        calories: {
+          type: Number,
+          required: true,
+        },
+        saturatedFats: {
+          type: Number,
           required: true,
         },
         createdBy: {
