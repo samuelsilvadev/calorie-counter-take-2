17088f83114aca57fcb7a0baafb41fa1d0e8f98f
Samuel Silva <samuelsilvawb@gmail.com>
2022-01-25 22:20:04 +0000

    chore(app): create check and close in circle icons

---
+++ app/src/shared/components/icons/Check.tsx
@@ -0,0 +1,14 @@
+function Check(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg width={19} height={16} fill="none" aria-hidden {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M7.855 12.448l-5.22-5.22a1.09 1.09 0 00-1.543 1.543l6.11 6.11a1.09 1.09 0 001.643-.117L18.01 2.546a1.09 1.09 0 10-1.745-1.31l-8.41 11.212z"
+        fill="#fff"
+      />
+    </svg>
+  );
+}
+
+export default Check;
